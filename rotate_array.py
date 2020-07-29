ddbd9e5b43525ea0d2bcff29839f4e3485bbf3ff
Yeonseo Ju <67610650+yunieju@users.noreply.github.com>
2020-07-28 22:54:58 -0500

    Create rotate_array.py

---
+++ rotate_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        arr = [0] * n
+        for i in range(n):
+            arr[(i+k) % n] = nums[i]
+        
+        nums[:] = arr
+        
+        
+    

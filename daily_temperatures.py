02db997119fed5e1fea129bf83bde55c760498fb
Yeonseo Ju <67610650+yunieju@users.noreply.github.com>
2020-08-06 14:41:18 -0500

    Update #739 Daily Temperatures

---
+++ daily_temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+       def dailyTemperatures(self, T):
+        res = [0] * len(T)
+        stack = []
+        for i in range(len(T)-1, -1,-1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
+            if stack:
+                res[i] = stack[-1] - i
+            
+            stack.append(i)
+        return res
+            

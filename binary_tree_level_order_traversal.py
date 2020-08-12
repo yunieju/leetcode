a4d75871fc068c068b949f81a57c6a24bf46fe04
Yeonseo Ju <67610650+yunieju@users.noreply.github.com>
2020-08-12 16:42:38 -0500

    update solution with dfs

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -1,6 +1,6 @@
 class Solution:
     # BFS + queue
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottom1(self, root: TreeNode) -> List[List[int]]:
         q = [(0,root)]
         res = []
         while q:
@@ -15,3 +15,17 @@ class Solution:
                 res[-(i+1)].append(node.val)
                     
         return res
+    
+    # DFS recursively
+    def levelOrderBottom2(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        self.dfs(0, root, res)
+                    
+        return res
+    def dfs(self, level: int, root: TreeNode, res)-> List[List[int]]:
+        if root:
+            if len(res) < level + 1:
+                res.insert(0, [])
+            res[-(level+1)].append(root.val)
+            self.dfs(level +1, root.left, res)
+            self.dfs(level +1, root.right, res)

7a13758998619698fab5dfda9674050ea2d32dc4
Yeonseo Ju <67610650+yunieju@users.noreply.github.com>
2020-08-22 23:34:34 -0500

    Create maximal_square.py

---
+++ maximal_square.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if matrix is None or len(matrix) < 1:
+            return 0
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0]*(cols+1) for _ in range(rows+1)]
+ 
+        maxlen = 0
+        for i in range(1,rows+1):
+            for j in range(1,cols+1):
+                if matrix[i-1][j-1] == '1':
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+                    maxlen = max(maxlen, dp[i][j])
+        return maxlen* maxlen
+        
